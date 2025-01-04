49ada64c4f3e7d25e23e53e25d1771eaea1ba307
AnshuByte <anshurauniyar2000@gmail.com>
2025-01-04 14:37:19 +0530

    changed folder Structure

---
+++ Basic_learning/src/component2/LearnReactPortal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LearnReactPortal = () => {
+  return (
+    <div>LearnReactPortal</div>
+  )
+}
+
+export default LearnReactPortal
---
+++ Basic_learning/src/components/PopupContent.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createPortal } from "react-dom";
+
+// This is to inject in the #root
+// const PopupContent = ({ copied }) => {
+//   return (
+//     <section>
+//       {copied && (
+//         <div style={{ position: "absolute", bottom: "3rem" }}>
+//           Copied to ClipBoard
+//         </div>
+//       )}
+//     </section>
+//   );
+// };
+
+// To directly attach to th body
+const PopupContent = ({ copied }) => {
+  // add CreatePortal
+  return createPortal(
+    <section>
+      {copied && (
+        <div style={{ position: "absolute", bottom: "3rem" }}>
+          Copied to ClipBoard
+        </div>
+      )}
+    </section>,
+    document.querySelector("#popup-content")
+  );
+};
+
+export default PopupContent;
